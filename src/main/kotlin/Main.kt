cf637b2ad9a83e63b1e57cf173b43be2a3504bea
Ryan OD <58502747+Ryan7od@users.noreply.github.com>
2025-12-03 15:16:35 +0000

    Commit working model for optimal5 and random with random fox

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,83 @@
+package org.example
+
+import kotlinx.coroutines.*
+
+suspend fun runMany(num: Int, iterations: Int, n: Int, farmer: Farmer, fox: Fox): Double = coroutineScope {
+    val jobs = List(num) {
+        async(Dispatchers.Default) { runSim(iterations, n, farmer, fox) }
+    }
+    jobs.sumOf { it.await() } / num
+}
+
+suspend fun main() {
+    val result = runMany(10, 1000000, 5, Optimal5Farmer(), RandomFox())
+    println(result)
+}
+
+fun runSim(iterations: Int, n: Int, farmer: Farmer, fox: Fox): Double {
+    val random = kotlin.random.Random
+    var totalDays = 0
+
+    repeat (iterations) {
+        var count = 0
+        var foxPos = random.nextInt(1, n)
+
+        for (i in 1..10000) {
+            // Move fox
+            if (foxPos == 1) foxPos++
+            else if (foxPos == n) foxPos--
+            else {
+                if (fox.leftOrRight()) {
+                    foxPos++
+                } else {
+                    foxPos--
+                }
+            }
+
+            // Farmer selects
+            val selection = farmer.next()
+
+            // If correct end
+            if (selection == foxPos) {
+                totalDays += i
+                break
+            }
+        }
+    }
+
+    return totalDays.toDouble() / iterations
+}
+
+abstract class Farmer(protected val n: Int) {
+    abstract fun next(): Int
+}
+
+class RandomFarmer(n: Int) : Farmer(n) {
+    private val random = kotlin.random.Random
+
+    override fun next(): Int {
+        return random.nextInt(1, n)
+    }
+}
+
+class Optimal5Farmer() : Farmer(5) {
+    private var index = -1
+
+    override fun next(): Int {
+        index = (index + 1) % (n-2)
+        return index + 2
+    }
+}
+
+abstract class Fox() {
+    // left == false, right == true
+    abstract fun leftOrRight(): Boolean
+}
+
+class RandomFox() : Fox() {
+    private val random = kotlin.random.Random
+
+    override fun leftOrRight(): Boolean {
+        return random.nextBoolean()
+    }
+}
